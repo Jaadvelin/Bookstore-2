da5af51ebc2f7c063da3a49ec530c41d149ed666
Jaadvelin <31605184+Jaadvelin@users.noreply.github.com>
2020-04-03 20:04:44 -0600

    Add files via upload
    Initial commit

---
+++ Booxstore 2/Booxstore 2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Booxstore 2
+//
+//  Created by user168030 on 3/31/20.
+//  Copyright © 2020 UwU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func addNew(_ sender: Any) {
+        
+    }
+
+    @IBOutlet weak var myTableView: UITableView!
+    
+}
+
---
+++ Booxstore 2/Booxstore 2Tests/Booxstore_2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Booxstore_2Tests.swift
+//  Booxstore 2Tests
+//
+//  Created by user168030 on 3/31/20.
+//  Copyright © 2020 UwU. All rights reserved.
+//
+
+import XCTest
+@testable import Booxstore_2
+
+class Booxstore_2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
